fabaad88b35386a2c73cfe38e47e90d7fdef79f4
fuxuelian395 <1531659183@qq.com>
2019-07-10 17:06:49 +0800

    修改header文件冲突

--- src/common/header/index.vue
+++ src/common/header/index.vue
@@ -1,73 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <header class="header">
-      <span class="jsback"  @click="prev">
-        <i class="iconfont icon-zuojiantou1"></i>
-      </span>
-      <h1>超级会员</h1>
-      <section class="ftr" id="navRight">
-        <span class="hd-other">
-          <i class="iconfont icon-tuichu"></i>
-        </span>
-      </section>
-    </header>
-  </div>
-</template>
-<script>
-export default {
-  name: "header",
-  data() {
-    return {
-     
-    };
-  },
-  methods:{
-    prev(){
-      console.log(111)
-      this.$router.back()
-    }
-  }
-};
-</script>
-<style>
-.header h1,
-h1 {
-  height: 0.9rem;
-  position: absolute;
-  top: 0;
-  right: 20%;
-  bottom: 0;
-  left: 20%;
-  text-align: center;
-  font-size: 0.35rem;
-  color: #333;
-  overflow: hidden;
-}
-.header {
-  padding: 0 0.32rem;
-   height: 0.9rem;
-  line-height: 0.9rem;
-}
-
-
-.header .ftr,
-.ftr {
-  /* height: 1.79rem; */
-}
-.ftr {
-  float: right;
-}
-.header i,
-i {
-  display: inline-block;
-  vertical-align: middle;
-  font-size: 0.4rem;
-  line-height: 1;
-  color: #00be88;
-}
-</style>
-=======
     <div class="header">
         <a href="javascript:;" @click="handleClick()"><i class="iconfont icon-zuojiantou1"></i></a>
         <a href="javascript:;"><i class="iconfont icon-ziyuan1"></i></a>
@@ -146,4 +77,3 @@ export default {
 </style>
 
 
->>>>>>> zhangbaoyin
--- src/common/index.js
+++ src/common/index.js
@@ -1,13 +1,7 @@
 import Vue from "vue";
 import Footer from "./footer";
-<<<<<<< HEAD
-Vue.component("TabBar",Footer);
-import Header from "./header";
-Vue.component("header",Header)
-=======
 import Header from "./header"
 
 Vue.component("TabBar",Footer);
 
 Vue.component("HeaderCom",Header)
->>>>>>> zhangbaoyin
