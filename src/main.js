cef35a9e855a717cdaca6d6d501613c5c1ec640f
heliang6 <3052618726@qq.com>
2019-07-11 19:05:49 +0800

    第二次修改main.js

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from "./router"
 import store from './store';
-<<<<<<< HEAD
-import "./common";
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
-Vue.use(MintUI)
-Vue.config.productionTip = false
-import VueTouch from "vue-touch"
-Vue.use(VueTouch,{name:'v-touch'})
-=======
 
 import "./common";
 import MintUI from 'mint-ui'
@@ -18,7 +9,6 @@ import 'mint-ui/lib/style.css'
 import VueTouch from "vue-touch"
 Vue.config.productionTip = false
 Vue.use(VueTouch,{name:"v-touch"})
->>>>>>> dev
 Vue.use(MintUI)
 new Vue({
     router,
